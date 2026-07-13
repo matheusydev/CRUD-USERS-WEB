d1cd221f801be1b26290207badac85c501e7a6fe
Matheusydev <matheusylanaraujo@gmail.com>
2026-07-13 20:47:22 -0300

    fix: corrige tag JSX do componente Home sem fechamento

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,35 @@
 import './style.css'
+import Trash from '../../assets/trash.svg'
 
 function Home() {
 
-  return (
-      <div>
+  const [users, setUsers] = useState([])
 
-      </div>
+  return (
+    <div className='container'>
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input name='nome' type="text" placeholder="Nome" />
+        <input name='idade' type="number" placeholder="Idade" />
+        <input name='email' type="email" placeholder="Email" />
+        <button type='button'>Cadastrar</button>
+      </form>
+
+      {users.map(user => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome:  <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Trash} alt="Excluir" />
+          </button>
+        </div>
+      ))}
+
+
+    </div>
   )
 }
 
