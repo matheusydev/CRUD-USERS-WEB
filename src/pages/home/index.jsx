9b842c037251ae0128c94e12e2d7b20dc73eb87a
Matheusydev <matheusylanaraujo@gmail.com>
2026-07-17 14:05:36 -0300

    style: ajusta tamanho e cor do ícone de excluir usuário

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './style.css'
 import Trash from '../../assets/trash.svg'
 import api from '../../services/api'
@@ -7,9 +7,34 @@ function Home() {
 
   const [users, setUsers] = useState([])
 
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
   async function getUsers() {
     const response = await api.get('/usuarios')
     setUsers(response.data)
+  }
+
+
+  async function createUsers() {
+    const response = await api.post('/usuarios', {
+      name: inputName.current.value,
+      age: Number(inputAge.current.value),
+      email: inputEmail.current.value
+    })
+    setUsers([...users, response.data])
+    inputName.current.value = ''
+    inputAge.current.value = ''
+    inputEmail.current.value = ''
+  }
+
+
+  async function deleteUsers(id) {
+    await api.delete(`/usuarios/${id}`)
+    setUsers(users.filter(user => user.id !== id))
+
+    getUsers()
   }
 
 
@@ -21,10 +46,10 @@ function Home() {
     <div className='container'>
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name='nome' type="text" placeholder="Nome" />
-        <input name='idade' type="number" placeholder="Idade" />
-        <input name='email' type="email" placeholder="Email" />
-        <button type='button'>Cadastrar</button>
+        <input name='nome' type="text" placeholder="Nome" ref={inputName} />
+        <input name='idade' type="number" placeholder="Idade" ref={inputAge} />
+        <input name='email' type="email" placeholder="Email" ref={inputEmail} />
+        <button type='button' onClick={createUsers}>Cadastrar</button>
       </form>
 
       {users.map(user => (
@@ -34,7 +59,7 @@ function Home() {
             <p>Idade: <span>{user.age}</span></p>
             <p>Email: <span>{user.email}</span></p>
           </div>
-          <button>
+          <button onClick={() => deleteUsers(user.id)}>
             <img src={Trash} alt="Excluir" />
           </button>
         </div>
